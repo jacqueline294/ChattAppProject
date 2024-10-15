e722f4e03d9f448fa031c152957125cde348d184
jacqueline294 <144172392+jacqueline294@users.noreply.github.com>
2024-10-15 19:33:48 +0200

    Initial Commit

---
+++ ChattAppProject/ChattAppProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChattAppProjectApp.swift
+//  ChattAppProject
+//
+//  Created by jacqueline Ngigi on 2024-10-15.
+//
+
+import SwiftUI
+
+@main
+struct ChattAppProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
